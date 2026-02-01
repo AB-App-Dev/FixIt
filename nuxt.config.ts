5e9b9331c2cdf77934e2694ceb2216f0606545f4
AB <ab@Aless-MacBook-Pro.local>
2026-02-01 14:24:56 +0100

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxtjs/leaflet'],
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      ],
+    },
+  },
   css: [
     '~/assets/css/main.css',
     'leaflet/dist/leaflet.css',
